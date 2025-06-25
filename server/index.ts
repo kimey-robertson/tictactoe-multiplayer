81766b59a71157bafa351b05b8b8e25d74cb9289
kimey-robertson <kimey@dealerstudio.com.au>
2025-06-25 16:22:22 +1000

    Serve React app from Express

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import path from "path";
 import {
   createGame,
   deleteGame,
@@ -14,6 +15,12 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 const PORT = 8000;
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
